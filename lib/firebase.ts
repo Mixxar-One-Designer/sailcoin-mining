c48935e32ab81fc927de71614729de36d64e20c1
Mixxar-One-Designer <muazzammixxa32@gmail.com>
2024-06-29 11:11:06 -0700

    updated Firebase file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBCIbR_IUO8DmWosjimObLuCE8CmQUniWU",
+  authDomain: "sailcoin-mining.firebaseapp.com",
+  projectId: "sailcoin-mining",
+  storageBucket: "sailcoin-mining.appspot.com",
+  messagingSenderId: "813284735552",
+  appId: "1:813284735552:web:5ca3a96bce3a3380ed3da5",
+  measurementId: "G-P7EHZ4TXNQ"
 };
 
 const app = initializeApp(firebaseConfig);
