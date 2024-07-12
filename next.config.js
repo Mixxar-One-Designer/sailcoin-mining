dbd3e1dadc4c9261b5e0ce053968002debf2eb36
Mixxar-One-Designer <muazzammixxa32@gmail.com>
2024-07-12 17:27:41 +0100

    Update page.tsx file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+// next.config.js
 module.exports = {
   images: {
-    domains: ['example.com'], // Add the domains where your images are hosted
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.telegram.org',
+        port: '',
+        pathname: '/file/bot6897920395:AAEl4SH-ZdkLdYwC8Ex9t7sp5jNhT2Ei2ws/**',
+      },
+    ],
   },
 };
